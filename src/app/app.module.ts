0537f294a3e304c9c159742fef962e57adffbeee
vikas magar <vikasmagar512@gmail.com>
2018-01-25 03:15:25 +0530

    Stock class to interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
